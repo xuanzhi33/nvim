6037829e26e3d7eb8a3eed363a5216e34143fb21
xuanzhi33 <xuanzhi33@qq.com>
2026-05-08 23:35:39 +0000

    add keymap to exit insert mode with 'jk'

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Map jk to <Esc> in insert mode
+vim.keymap.set("i", "jk", "<esc>", { desc = "Exit insert mode", remap = false })
+
